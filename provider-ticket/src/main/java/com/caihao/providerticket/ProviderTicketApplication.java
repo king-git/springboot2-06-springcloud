39ad68c56e8d1937a57f65bc8bc1cfe14a2c3c7a
caihao <kingch92@163.com>
2019-09-09 21:07:00 +0800

    更新

--- provider-ticket/src/main/java/com/caihao/providerticket/ProviderTicketApplication.java
+++ provider-ticket/src/main/java/com/caihao/providerticket/ProviderTicketApplication.java
@@ -3,6 +3,10 @@ package com.caihao.providerticket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 同一个应用可以部署多个实例，注册在注册中心上
+ */
+
 @SpringBootApplication
 public class ProviderTicketApplication {
 
